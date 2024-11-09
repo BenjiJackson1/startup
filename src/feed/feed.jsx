9fc9b05df3258b9a2373b39fd44db9ab425a64ee
BenjiJackson1 <blj55@cornell.edu>
2024-11-08 19:58:31 -0700

    Removal of static post content from feed

--- src/feed/feed.jsx
+++ src/feed/feed.jsx
@@ -38,40 +38,6 @@ export function Feed() {
           </div>
         </div>
       ))}
-      <div className="workout-container mt-3">
-        <h4> Post by:UserX32 </h4>
-        <p> 9/23/2024 at 12:07 PM</p>
-        <h4> Dumb </h4>
-        <p> Sets: 3 </p>
-        <ul className="workout">
-          <li> 10 reps, 80 lbs </li>
-          <li> 12 reps, 80 lbs</li>
-          <li> 12 reps, 75 lbs</li>
-        </ul>
-
-        <h4> Chest Fly </h4>
-        <p> Sets: 2 </p>
-        <ul className="workout">
-          <li> 10 reps, 100 lbs </li>
-          <li> 10 reps, 110 lbs</li>
-
-        </ul>
-        <p className="btn btn-dark"> Like This Post ❤️(5)</p>
-      </div>
-      <hr />
-
-      <div className="workout-container mb-3">
-        <h4> Post by: JJean48</h4>
-        <p> 9/21/2024 at 11:50 PM</p>
-        <h4> Leg Press </h4>
-        <p> Sets: 3 </p>
-        <ul className="workout">
-          <li> 10 reps, 130 lbs </li>
-          <li> 12 reps, 140 lbs</li>
-          <li> 12 reps, 120 lbs</li>
-        </ul>
-        <p className="btn btn-dark"> Like This Post ❤️(11)</p>
-      </div>
     </main>
   );
 }
